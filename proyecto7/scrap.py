964271464304577d3af4e13dd4eed163b4984f6d
Omar Mancilla <omarciano19@gmail.com>
2024-07-28 00:29:10 -0600

    Update scrap.py
    Corregido la creación del csv

--- proyecto7/scrap.py
+++ proyecto7/scrap.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import re
 from bs4 import BeautifulSoup
@@ -5,8 +6,7 @@ import pandas as pd
 
 URL = "https://practicum-content.s3.us-west-1.amazonaws.com/data-analyst-eng/moved_chicago_weather_2017.html"
 req = requests.get(URL)
-soup = BeautifulSoup(req.text, 'lxml')
-
+soup = BeautifulSoup(req.text, 'html.parser')
 
 table =  soup.find_all("table", attrs={"id": "weather_records"})
 #Encabezados:
@@ -23,6 +23,15 @@ for row in table[0].find_all('tr'):
         content.append([element.text for element in row.find_all('td')])
 #print(content)
 
-weather_records=pd.DataFrame(content, columns=heading_table)
+weather_records = pd.DataFrame(content, columns=heading_table)
 print(weather_records)
-weather_records.to_csv('datos.csv')
+#weather_records.to_csv('./datasets/datos.csv', index=False)
+directorio_actual = os.path.dirname(os.path.abspath(__file__))
+print("directorio actual:", directorio_actual)
+# Crear la ruta completa para el archivo CSV
+ruta_csv = os.path.join(directorio_actual, 'datos.csv')
+print("ruta_csv",ruta_csv)
+# Guardar el DataFrame como CSV en el directorio actual
+weather_records.to_csv(ruta_csv, index=False)
+
+print(f"Archivo CSV guardado en: {ruta_csv}")
